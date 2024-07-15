409ae9de66014496ac5224b7133c175ea912a53e
Antonio Escobedo <escovelas@gmail.com>
2024-07-15 09:09:07 -0600

    add cards, fix circle

--- src/cards.js
+++ src/cards.js
@@ -13,6 +13,45 @@ import valiente from "./resources/cards/valiente.mp3";
 import gorrito from "./resources/cards/gorrito.mp3";
 import muerte from "./resources/cards/muerte.mp3";
 import pera from "./resources/cards/pera.mp3";
+import bandera from "./resources/cards/bandera.mp3";
+import bandolon from "./resources/cards/bandolon.mp3";
+import violoncello from "./resources/cards/violoncello.mp3";
+import garza from "./resources/cards/garza.mp3";
+import pajaro from "./resources/cards/pajaro.mp3";
+import mano from "./resources/cards/mano.mp3";
+import bota from "./resources/cards/bota.mp3";
+import luna from "./resources/cards/luna.mp3";
+import cotorro from "./resources/cards/cotorro.mp3";
+import borracho from "./resources/cards/borracho.mp3";
+import negrito from "./resources/cards/negrito.mp3";
+import corazon from "./resources/cards/corazon.mp3";
+import sandia from "./resources/cards/sandia.mp3";
+import tambor from "./resources/cards/tambor.mp3";
+import camaron from "./resources/cards/camaron.mp3";
+import jaras from "./resources/cards/jaras.mp3";
+import musico from "./resources/cards/musico.mp3";
+import arana from "./resources/cards/arana.mp3";
+import soldado from "./resources/cards/soldado.mp3";
+import estrella from "./resources/cards/estrella.mp3";
+import cazo from "./resources/cards/cazo.mp3";
+import mundo from "./resources/cards/mundo.mp3";
+import apache from "./resources/cards/apache.mp3";
+import nopal from "./resources/cards/nopal.mp3";
+import alacran from "./resources/cards/alacran.mp3";
+import rosa from "./resources/cards/rosa.mp3";
+import calavera from "./resources/cards/calavera.mp3";
+import campana from "./resources/cards/campana.mp3";
+import cantarito from "./resources/cards/cantarito.mp3";
+import venado from "./resources/cards/venado.mp3";
+import sol from "./resources/cards/sol.mp3";
+import corona from "./resources/cards/corona.mp3";
+import chalupa from "./resources/cards/chalupa.mp3";
+import pino from "./resources/cards/pino.mp3";
+import pescado from "./resources/cards/pescado.mp3";
+import palma from "./resources/cards/palma.mp3";
+import maceta from "./resources/cards/maceta.mp3";
+import arpa from "./resources/cards/arpa.mp3";
+import rana from "./resources/cards/rana.mp3";
 
 
 const cards = [
@@ -105,6 +144,240 @@ const cards = [
     title: "La pera",
     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi8DkNIeeFvuZAyUec-TscLnsc5epuYjU_CXsiN7eXv4u45XespkeSenZdlaaB4hW5aIn6ObnqA-dUT2Udkd_EaWr8qgX95HhseJG3tptvd6CwBpZM-R4Ti6d7szivF-k4rqz_v_8Ci_gDt/s2048/15+la+pera-min.jpg",
     audio: pera,
+  },
+  {
+    number: 16,
+    title: "La bandera",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg1RjqNRH7AW991XJrxrvQUDm-q97gga_kF-1yYG8ThJF-k3_9do03iYye5wvs7qiyJXyBPzUCyDAdqye9WB9ICv1KTJ4Flz8Ck-ER12FzOlu9U4v-Ue7fh-9Vsolz11r1FaTzwo4ARi13l/s2048/16+la+bandera-min.jpg",
+    audio: bandera,
+  },
+  {
+    number: 17,
+    title: "El bandolón",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3GkKRILWjQAt46mGvVt1ulGn0pKpZuWN6FVfyMeyIbmdWSbfYgBzwIZtz70KQ5b1S4CjoVyZ1s7fl84MNHdBcNTj5nH_oRA_i1QZlABOMeGjM5ElQsoe0t59BX1pROqe8x49VAu-cq05D/s2048/17+el+bandolon-min.jpg",
+    audio: bandolon,
+  },
+  {
+    number: 18,
+    title: "El violoncello",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEguMAHAJPPq8xK_Jlrb_5LSINBhZPoTf_fwplvkwsA1A8fHRy1_iKAbbbGeln6uzKm8hTGBrw3ouMpK2WgELS5lMnQojy8Baad2IMIyeekThNojfcguUC2UIj8OybX1C-f0vREYhKiHWEXd/s2048/18+el+violoncello-min.jpg",
+    audio: violoncello,
+  },
+  {
+    number: 19,
+    title: "La garza",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjZcoAZAgxSgz8Do7CDVVXnLE6NC82swUO_bk-rPc9S9L3-SGbfliXyRCOQZZbNZUrpXzBowmWxhRaTFCmPGPB4dzrQxrXAnNAU6UVqi96dMCSiwQz4klp3qQKRpI8HRenSaadzkI7RIrey/s2048/19+la+garza-min.jpg",
+    audio: garza,
+  },
+  {
+    number: 20,
+    title: "El pájaro",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj3zxC50As3kpiHsM4ui07lKcRqN8UuARlx__BVoyEBWY7EFgGOBqWdKpLLab372mQqGGbvegXbFmc83lsY3HonfYJFmNdJqQolxjZjb8HBoof6me_AajGCn6czsnZU8zUJ9CJTUkNnDNhi/s2048/20+el+pajaro-min.jpg",
+    audio: pajaro,
+  },
+  {
+    number: 21,
+    title: "La mano",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEia21R6vIJ-4_r9yYAZuOaoeT2dKdjjvN0X3e7bm4kRlNksjSvCymox1VdnIIQPHBs0YyrNBFKUcv-eJzx6_47yzSjoiWZ5ZQ2r9gNAlaWr-AqDk7htx1OPMDdOv4y4Eq6KNSQD4ibV7od1/s2048/21+la+mano-min.jpg",
+    audio: mano,
+  },
+  {
+    number: 22,
+    title: "La bota",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhYFq6OGdN-a3ym8TO6P38pz638t6AfuduHvfK1DX7XqeUWGxmlIFD0_glN4DNYA2EKUio9_aFJCWnVsgiIAC9VvJKsgb1NxgSSO0prrgmODjJrA3njznF-UwhZ4WBTqwkWOVQqyGpWuy2b/s2048/22+la+bota-min.jpg",
+    audio: bota,
+  },
+  {
+    number: 23,
+    title: "La luna",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgBfqzdpoLgfvms1MVWEK4ekwVBiwp80KvP4DPrcic3Rld_869dEtW6GjuaAMcxFNXbMNEsPI3rVPsZr8avUIkSJbBk0Oi2JqZhhM_woKbu2B2XgyhxLdyz8DPJfJnZaIxYOa_9e5WXZWNF/s2048/23+la+luna-min.jpg",
+    audio: luna,
+  },
+  {
+    number: 24,
+    title: "El cotorro",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh1I7X5aQOOcLdoO_N9Swr20ZVnWTJvlABBkiB87DhviGfWYN7NCfTYfVnD6DoWjV8wbHWfhNyMsGOuh2D1XeQJ_ZcrXQEOetxSBMgK0F4K75la2D4Ki5tFe3JRcUhmDDtXtObpIWTW8PC2/s2048/24+el+cotorro-min.jpg",
+    audio: cotorro,
+  },
+  {
+    number: 25,
+    title: "El borracho",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjp6UwZ4Hncizaevz7emJtg7wpvQ6U-nW3fDCeZjyDhpNXteujksDE69viKg_idCjKCuG1tDwI02_ZtakSRo9qigKBSU2oe6oouVmm3M_8ipXvFHZuuSCLN81Cgi-SwOGqMpWTsFDFA-tWX/s2048/25+el+borracho-min.jpg",
+    audio: borracho,
+  },
+  {
+    number: 26,
+    title: "El negrito",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjrDDWh583kNskNlMI3XXZsLB2KAFlOwHrEpEtbX3xdTF3kT3-KB_mkELHrwnnvSY_bYQxbuSWO2dGIuv1mtfPd1gJ0ZdzuINdimLAhiAhE89_bo22bjpVuZ8dmK2Rrvd4hySsdmnKm5n2g/s2048/26+el+negrito-min.jpg",
+    audio: negrito,
+  },
+  {
+    number: 27,
+    title: "El corazón",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjVXGWM84w7MNPKBn80w4HOc6ZXpG4w8C20jtmWLE9hMzV9UIVVXN3j8SyplaaLHFovLpp63g8C9Ay-74PPHw3VTMRbyAUnK3wm7gjQGHvazGimCmCMSGY3TtjGLuXFT1bsqEGVagwtVrRM/s2048/27+el+corazon-min.jpg",
+    audio: corazon,
+  },
+  {
+    number: 28,
+    title: "La sandía",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj-omCEw85_JeEH2ggAikwetrHNcZXTWS2_kG29Sf4gi4iTtDbNGnCaYl38MsRNmJXWYMzBBnXfCQFljZusH7bbEWPjV6BEYkYa7VAe3uOuFW3j2Pr1FTeNJUv4XTP8c_HrjIhkbX3i40xc/s2048/28+la+sandia-min.jpg",
+    audio: sandia,
+  },
+  {
+    number: 29,
+    title: "El tambor",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgStZoGr6AsJJZrvtNvVUuYziMHBCWfaywHYYehufwD4fNlxJQ1_bb5UvMEDDhRRx096SKqFxVjL3cpzt-zjbA6eW1ZagKml0kkM8tvg3YDuLCyLb3QTMKB3_tk_M6nUbIENXhwSwhIG5WR/s2048/29+el+tambor-min.jpg",
+    audio: tambor,
+  },
+  {
+    number: 30,
+    title: "El camarón",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgeu_duCzE4TwHCI7wrYF13ljYfNq0IynYXuaNytpkbPbLNjGjCbn_Bj2FSLAjWiOoUo03xMN2KVr6YsTGxmNJ6esZUG6zTDYA3jBbe5HUrHDS4PLiL7uTvHixtDsW2vQ7SDwBlSf2X2q__/s2048/30+el+camaron-min.jpg",
+    audio: camaron,
+  },
+  {
+    number: 31,
+    title: "Las jaras",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgGYkU6c7q6eam6BKcajbmhZBFbz5Aqr5WScoeb8UF40tt-i5hrR8xfG8nsmL3g72Vt2lQuQRnBfD9oZsW-xz-epBNcatqqQj3EXuQ6xnbZeIL-RnB-Yv6lKJYAfAoQ7dzHSBoguc_MxlbU/s2048/31+las+jaras-min.jpg",
+    audio: jaras,
+  },
+  {
+    number: 32,
+    title: "El músico",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgImh9pyYwkkzV0h2_YBRdZ7dI2Agk1hXotdl-2uILapDbhWhTbuw969ZRw9feQiUOgbaXbAKykAdqJiskJo9G6MXQfoVKr2DFcEldVEgA66f_4pKsAVJzEXoVNRvgnTQduI0Mmt28SV_jL/s2048/32+el+musico-min.jpg",
+    audio: musico,
+  },
+  {
+    number: 33,
+    title: "La araña",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhc5tOM5K8uuvzoOTjPVNjzkhckGLFQ8jHlAd847zyspIqpV0M7MnIrNlk3QsrG7iPES9HNhHSsjk-A9fChRXMpgglPH2j3eIpPb2dUdtGlCl5wGDy6AFRtKsIL2ObNyKmqVK2jooYrMhwe/s2048/33+la+ara%252B%25C2%25A6a-min.jpg",
+    audio: arana,
+  },
+  {
+    number: 34,
+    title: "El soldado",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgfj2_cHUu9xTmNTtX12WTQCLp098_g5zvjILJ3UK8X8kwYmpuwq5zkhszgUzU4GMG2YcWSiyyuCksNbKayOW_7y2z5y8oBYJc6QdhSTUdboChQyFwrmNViVkqo7YrlXcuD1HL8hKldYYVg/s2048/34+el+soldado-min.jpg",
+    audio: soldado,
+  },
+  {
+    number: 35,
+    title: "La estrella",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgfEqXLwblss7nemewEawbbymggpTi-Tqvucf1gRVW2TjLLcWlZsOX1IG2_CLNTQj0cMkR0kvXyRuNYn0ubJznOF3_moEfzwQ5vUYXE7XL4pPLy9Dmt2WaQiWHzsNsOIwk-Wbj4mWDOK5dE/s2048/35+la+estrella-min.jpg",
+    audio: estrella,
+  },
+  {
+    number: 36,
+    title: "El cazo",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjtFnlY1bUkb9YGgl2mRDGl7BR54SLNWEio8X28YlDFkwJq724QyS1Omeu5aquJZHddLMy7leX48T634X_iXWBaMxiTqc-gk8FGcm3rH8TzL3jzl9gCwl8zc4Wpe7cLxhyphenhyphenIX05NeoacQQS9/s2048/36+el+cazo-min.jpg",
+    audio: cazo,
+  },
+  {
+    number: 37,
+    title: "El mundo",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEior8RblWDG-49D2lpyhKc4AKw2DQybSufv2BfWPY0ncdFDjU77U4_MgxuUqvHqZJoZOjAURF93oRDtsdtDFaSv-RayZYWjFiSnEH4Uy8CuEF4-YxNDpOaNd36tsKhEMjzfIIlY2nFudqux/s2048/37+el+mundo-min.jpg",
+    audio: mundo,
+  },
+  {
+    number: 38,
+    title: "El apache",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi6NiBqJuDFCiDFjVo3DbWbB6BZTCwJTZqh3V4FoegQjg6dOjdWqbbpL6fYH9p0Dy5QUUpTMq30emFdOuK4SfPVjHmOBXjj3tv_jotCXfMA3edLtOLf4uqkVh0yHYR1a7kE4BHp2ZTyAqGb/s2048/38+el+apache-min.jpg",
+    audio: apache,
+  },
+  {
+    number: 39,
+    title: "El nopal",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiYZPh1GRMuv1-3T9GpCAYemF60eYtx5PBMqQjNUN_7UZrHQyXGjxAETxCoM4VnUaHuSzveu12v0Nwd5IBRleM-LJo9cZZ40W46bnByHAdX7UplKPdeOgLD3sf0uTuxIJhMUH1DMxa6OCbb/s2048/39+el+nopal-min.jpg",
+    audio: nopal,
+  },
+  {
+    number: 40,
+    title: "El alacrán",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQG1zt3wSNml4Kbst_Dx9KEV3dqOc3rpf06diPLyXUPx7vv6VoNIN1JqyXc_woI3AmpnBueW9HYTghWLl7hajFW_ok1dQT2X8DyfquEYbNzMcaNCcrJMHf9VQqRw05VAsei0LmuaKdDKyb/s2048/40+el+alacran-min.jpg",
+    audio: alacran,
+  },
+  {
+    number: 41,
+    title: "La rosa",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiVnBueNWASf-MHWd8-NMJ_DIsXD_e2SLC9TFR6CBKjuUkrXPSa7GfJ1pKGAC9UH8auN6AqZ8gVg5sEDpDbZ1J_0wLbnjLsNChV4SB7-YWnU-UOl_6RZBTH0u36jpZdfgzZeKQH5C-HU7h8/s2048/41+la+rosa-min.jpg",
+    audio: rosa,
+  },
+  {
+    number: 42,
+    title: "La calavera",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhPYXfezg6HFfe8bqJ7nJf9kKR_oP9Y4qpiErJh9SRsiZ9hk3TFjiCPVi_t2HD5BI1dlB2BC2-n_CQobek3iqJssEEnGwC81LGVmMQJF8pQCd0ghuKw_naQuO3t1-rpNl_vnlUteehINXal/s2048/42+la+calavera-min.jpg",
+    audio: calavera,
+  },
+  {
+    number: 43,
+    title: "La campana",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiyT8SvE__Gx4Fu-TJsd4obi3xMuXcs2xVLWl5dpnbtmZo8ZhwAwoVCqSosNx8g9WJs5EzOyajuZgsseidnCv1Z10WBv5NiD4GL5fbx0gdSc68anEHL2iJ-1aNTWr1Ch6TX3S6R2RlOfkJl/s2048/43+la+campana-min.jpg",
+    audio: campana,
+  },
+  {
+    number: 44,
+    title: "El cantarito",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicJ5YS5TqqsZ4FN0Fb8StFO5-sFN0S222njfXz3NCfTZ84GIKWmdsuoNbFflBZjX78us_1YZ0Fv6fWchDD85aJrwLlS6ucsn2Dswem04zPDDuZbmNBUgjABHxjDk3WVSBxAZYZV6xz3fhZ/s2048/44+el+cantarito-min.jpg",
+    audio: cantarito,
+  },
+  {
+    number: 45,
+    title: "El venado",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjprhgMXQVT68CgLVI6GyI6fcoKSX3og-DQYwxhqsesnOQKBxIk3EUiq-5ryIjWJE3SeJfAu5EzDHlhVt7GS0jWzay9SgxWJRdmbQxjKc1PT2wEmvCC1zy2WndHusPnMDf6GYn_TRTs_zJQ/s2048/45+el+venado-min.jpg",
+    audio: venado,
+  },
+  {
+    number: 46,
+    title: "El sol",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhQF3wZxWFwmvMkyzkKetQ8zvF5G7FrLsmMy3U5QuOje_5HCTVI7uWEBJlirKVlXIVxyG97pMc47COvQ02guQyRSIJ_6CgHLzJaqFIzqtX3pmbkXFNwj-vZR251jJDM2EQyWqoBvc2fo6Qj/s2048/46+el+sol-min.jpg",
+    audio: sol,
+  },
+  {
+    number: 47,
+    title: "La corona",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiB9zdoLaA8gjrQRUklFwAWe1XU6Gw1rbkiD1JGS4Fi3R7fOUYd61JTvW8X7Lt98OYeb06WyQBV2tUdOTUZ2zuVenkoCogxsJkn0k_y9KdhEETM8wbnoZOdD38MpV9AaU49RiywKk2WMmK7/s2048/47+la+corona-min.jpg",
+    audio: corona,
+  },
+  {
+    number: 48,
+    title: "La chalupa",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh1Ef6jKsFjv4tOqphNDNg1ZN83JCX5JiFCFFdN5klDaP5gzKEgb4A0X0vtCNYlpkgjwY4Cak7KitnB7mvtufG41Ruy-nt_N2Eb5leE2uxLJUsnBYTm_5kY5niMkPFv5CdDp2H2GZIFemRr/s2048/48+la+chalupa-min.jpg",
+    audio: chalupa,
+  },
+  {
+    number: 49,
+    title: "El pino",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjs9bGe6KE2Wuot6Xte1ceNx1h8u6DNb1uWLtNJeSHchqWF4RcK0aP6-mHBQL7f71TESo8HdfTNdbBkLN9tr0npK5LIS3PsnEz9m0K9Yi1ai57E5SMCiuYOyajjKoKaLw-Nu170-NdvN260/s2048/49+el+pino-min.jpg",
+    audio: pino,
+  },
+  {
+    number: 50,
+    title: "El pescado",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3bYNCwtTgqcMKzv_uBR0d0ZNSOXL6_BHRLbsnJnJe-CRBtiK_-_4A_onK_wsVCL3twp73OAeOyuO_NIYYlxAODNtKda_IS4YZeXLFfse37dq2yZztGcxASUzEIdkSY8uW3VsofUEHlpSN/s2048/50+el+pescado-min.jpg",
+    audio: pescado,
+  },
+  {
+    number: 51,
+    title: "La palma",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiSZFahdi4nGW-ut-gbGYaY5nQlt8u6FLUDnVvRxafLvleM5prxwGV7L1uprUEvvJ4fOgQmuCnp53fSLGF497KrQW48qJMl3crXE3t_nZPlakaZOWRn6o-nIGqwZmqznE7r36xSAWUt2sP1/s2048/51+la+palma-min.jpg",
+    audio: palma,
+  },
+  {
+    number: 52,
+    title: "La maceta",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinbLnHhry1czAT_xXKrZB8WxHe82FekzN1ULJbggZj2kQQi5Dd4h3fVHkSAzeOtp3l3YI5i0btWazV7r8CC1FGJTV_KUbeCU-PyJ1HNUV7SKquLYOUf_4tafhZcSK_dPudmzilrwpn8txn/s2048/52+la+maceta-min.jpg",
+    audio: maceta,
+  },
+  {
+    number: 53,
+    title: "El arpa",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhhz61xTjYlPXKQVj6mv4pgSdQbGIZVcyEMGcnJenjP7kdp39QHEM7YPDfk1dZCfWU7sYA-rIigf9ANEATY7bMFGcnC4zZRVxg_39T2AujLAPBc-6tQFL_GZVQYpa4Jc4ltBMaqubTbdz9G/s2048/53+el+arpa-min.jpg",
+    audio: arpa,
+  },
+  {
+    number: 54,
+    title: "La rana",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhM6az5KeQJfK0-kEWZVUw-aFv9jxqgUyePyx3syLasUsnCrv3I6QUBeASqPKP0rQonp8bR40gkriiEWfh5xTbTjPBqRgBeI8zQEXOAmEgZPFP7VOTe_DGbiWVvYRh1qtmcYiB6ak5PKnh8/s2048/54+la+rana-min.jpg",
+    audio: rana,
   },
 ];
 
